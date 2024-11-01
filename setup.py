b9b7c5bc440045bf6c996a7fac295b8b8a02a1a3
Jermy <penajermy24@gmail.com>
2024-11-01 16:27:32 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,9 +6,9 @@ setup(
     description="A Python client for accessing the RBXStats API.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    author="Your Name",
-    author_email="your.email@example.com",
-    url="https://github.com/yourusername/rbxstats_api",  # Replace with your repo URL
+    author="JermyOIS",
+    author_email="rbxstatsxyz@gmail.com",
+    url="https://github.com/Jermy-tech/rbxstats_api",  # Replace with your repo URL
     packages=find_packages(),
     install_requires=[
         "requests>=2.20.0"
