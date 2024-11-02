3b245d50735d0a80860ba3d1e8ed7b52862390d8
Jermy <penajermy24@gmail.com>
2024-11-02 14:40:47 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="rbxstats_api",
-    version="0.1.0",
+    version="0.1.1",
     description="A Python client for accessing the RBXStats API.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
